2ce98d42352f7a78b8a8ba5468080bced9ce61f5
Gabriel <gabrielvt6@gmail.com>
2024-06-19 22:01:49 -0700

    Ok this is more like it

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import './App.css';
-import FileUploader from './components/FileUploader';
+import React, { useState } from "react";
 
-function App() {
+import './App.css';
+import File from './components/File';
+
+export default function App() {
+  const [files, setFiles] = useState([]);
+
+  function handleChange(e) {
+    const file = URL.createObjectURL(e.target.files[0]);
+    setFiles([...files, file]);
+    e.target.value = null;
+  }
+
   return (
     <div>
-      <header>
+      <main>
         <h1>Work in progress</h1>
-        <FileUploader />
-      </header>
+        <input type="file" onChange={handleChange} />
+        { 
+          files.map ((fileObject, index) => {
+            return <File key={index} file={fileObject} />;
+          })
+        }
+      </main>
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/components/File.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+
+export default function File({ file }) {
+
+  return (<img src={file} />);
+};
