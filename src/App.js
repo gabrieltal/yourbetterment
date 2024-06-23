6d41f785ab93b192cf50956de0d84211ff8b5dd2
Gabriel <gabrielvt6@gmail.com>
2024-06-22 22:56:22 -0700

    Add drag/drop functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import './App.css';
 import File from './components/File';
@@ -6,6 +6,18 @@ import File from './components/File';
 export default function App() {
   const [files, setFiles] = useState([]);
   const [selected, setSelected] = useState(null);
+  const [bettermentShape, setBettermentShape] = useState({ top: null, left: null });
+  const bettermentShapeRef = useRef(null);
+
+  useEffect(() => {
+    if (bettermentShapeRef.current !== null) {
+      const rectangle = bettermentShapeRef.current.getBoundingClientRect();
+      setBettermentShape({
+        top: rectangle.top,
+        left: rectangle.left,
+      });
+    }
+  }, [bettermentShapeRef.current])
 
   function handleChange(e) {
     const file = URL.createObjectURL(e.target.files[0]);
@@ -14,15 +26,25 @@ export default function App() {
   }
 
   return (
-    <div>
-      <main>
+    <div className="mainContent">
+      <header>
         <h1>Work in progress</h1>
         <input type="file" onChange={handleChange} />
-        {
-          files.map ((fileObject, index) => {
-            return <File key={index} file={fileObject} isSelected={selected == fileObject} setSelected={() => setSelected(fileObject)} />;
-          })
-        }
+      </header>
+      <main>
+        <div className="bettermentShape" ref={bettermentShapeRef}>
+          {
+            files.map ((fileObject, index) => {
+              return (
+                <File key={index}
+                      file={fileObject}
+                      isSelected={selected == fileObject}
+                      setSelected={() => setSelected(fileObject)}
+                      bettermentShape={bettermentShape} />
+              );
+            })
+          }
+        </div>
       </main>
     </div>
   );
--- src/components/File.js
+++ src/components/File.js
@@ -1,6 +1,20 @@
 import React, { useState } from "react";
 
-export default function File({ file, isSelected, setSelected}) {
-  const className = isSelected ? 'selected' : '';
-  return (<img src={file} className={className} onClick={setSelected} />);
+export default function File({ file, isSelected, setSelected, bettermentShape }) {
+  const className = isSelected ? 'selected image' : 'image';
+
+  function handleDrop(e) {
+    const fileCenterX = e.target.width / 2;
+    const fileCenterY = e.target.height / 2;
+
+    // e.client(X, Y) = get mouse position
+    // fileCenter(X, Y) = get X, Y coordinates of the center of the image
+    // bettermentShape(top, left) = get top left corner of the drop center (e.g. the betterment logo shape)
+    // e.target.style (left, top) = writers for the file's position
+    e.target.style.left = e.clientX - fileCenterX - bettermentShape.left + 'px';
+    e.target.style.top = e.clientY - fileCenterY - bettermentShape.top + 'px';
+    return false;
+  }
+
+  return (<img src={file} className={className} onClick={setSelected} draggable={isSelected} onDragEnd={handleDrop} />);
 };
